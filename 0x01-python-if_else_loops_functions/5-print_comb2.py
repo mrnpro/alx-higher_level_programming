8bad944219d2e56707ca7e3a976209c13f47ae62
mrnpro <yegetanaty03@gmail.com>
2021-11-17 12:07:52 +0300

    editting on print

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-for i in range(100):
-	if i >10:
-		print('0', i, end=", ")
-	else:
-		print(i, end=", ")
+for i in range(0, 99):
+    if i < 10:
+       print('0', end="")
+    print(i, end=", ")
 
