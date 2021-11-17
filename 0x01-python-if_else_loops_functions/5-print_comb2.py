b3351f22f3d181e8340c3cfdde0fce12cf1285e1
mrnpro <yegetanaty03@gmail.com>
2021-11-17 12:52:24 +0300

    formatting

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
-for i in range(0, 99):
+for i in range(0, 100):
     if i < 10:
        print('0', end="")
-    print(i, end=", ")
+    print("{}".format(i), end=", " if i < 98 else ', {}\n'.format(99))
 
