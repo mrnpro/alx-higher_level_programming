44d31ba3b97ffd9c63fc64f9a974ff93e37a108a
mrnpro <yegetanaty03@gmail.com>
2021-11-17 11:26:22 +0300

    removing tabs

--- 0x01-python-if_else_loops_functions/3-print_alphabt.py
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(ord('a'), ord('z')+1):
-	if i is 101 or i is 113:
-		continue
-	else:
-		print("{:c}".format(i), end="")
+    if i is 101 or i is 113:
+	continue
+    else:
+	print("{:c}".format(i), end="")
