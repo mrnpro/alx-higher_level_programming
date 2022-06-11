361453275f251a3e078aadddf0f1d1a551ea9a2b
Natnael Fikadu <88460514+mrnpro@users.noreply.github.com>
2022-06-12 00:55:16 +0300

    Update 0-square.py

--- 0x06-python-classes/0-square.py
+++ 0x06-python-classes/0-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """empty class that defines a square"""
 
-class Square:
-      """square class with an artribute"""
+  class Square:
+    """square class with an artribute"""
     pass
