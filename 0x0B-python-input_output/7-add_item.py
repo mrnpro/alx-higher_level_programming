4c571dccf6c33c18ffa439cc41f89daf3a299df9
Natnael Fikadu <88460514+mrnpro@users.noreply.github.com>
2022-06-13 18:58:36 +0300

    Update 7-add_item.py

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,26 +1,15 @@
 #!/usr/bin/python3
+"""load and save"""
 import sys
-save_to_json = __import__("7-save_to_json_file").save_to_json_file
-load_from_json = __import__("8-load_from_json_file").load_from_json_file
-
-
-def add_item(args, filename):
-    '''
-        adds items to a json file
-        @args: The arguments that need to be added.
-        @filename: The file that needs to be updated
-    '''
-    try:
-        content = load_from_json(filename)
-    except:
-        content = []
-
-    for item in args:
-        content.append(item)
-    save_to_json(content, filename)
-
 
 if __name__ == "__main__":
-    args = sys.argv[1:]
-    filename = "add_item.json"
-    add_item(args, filename)
+    save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+    load_from_json_file =\
+        __import__('6-load_from_json_file').load_from_json_file
+
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        item = []
+    item.extend(sys.argv[1:])
+    save_to_json_file(item, "add_item.json")
