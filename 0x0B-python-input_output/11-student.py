4ff8237a650b4eaada0af914cb1e5eba028b96fc
Natnael Fikadu <88460514+mrnpro@users.noreply.github.com>
2022-06-13 18:50:40 +0300

    Update 11-student.py

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,42 +1,29 @@
 #!/usr/bin/python3
-'''
-    Defines a student class
-'''
+"""disk and reload"""
 
 
 class Student:
-    '''
-        Defines a student class
-    '''
+    """defines a student"""
+
     def __init__(self, first_name, last_name, age):
-        '''
-            Initializing instance variables
-        '''
+        """Initializes a new Student"""
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
-
+        
     def to_json(self, attrs=None):
-        '''
-            Retrives the dict representation of the class
-        '''
-        my_dict = {}
-        if type(attrs) == list:
-            for i in attrs:
-                if type(i) != str:
-                        my_dict = self.__dict__
-                        break
-                try:
-                    my_dict[i] = getattr(self, i)
-                except:
-                    pass
-        else:
-            my_dict = self.__dict__
-        return (my_dict)
-
+        """retrieves a dictionary representation of a Student instance"""
+        if attrs is None:
+            return self.__dict__
+        new_dict = {}
+        for a in attrs:
+            try:
+                new_dict[a] = self.__dict__[a]
+            except:
+                pass
+        return new_dict
+    
     def reload_from_json(self, json):
-        '''
-            Replaces all attributes of student instance
-        '''
-        for key, val in json.items():
-            self.__setattr__(key, val)
+        """replaces all attributes of the Student instance"""
+        for k, v in json.items():
+            setattr(self, k, v)
