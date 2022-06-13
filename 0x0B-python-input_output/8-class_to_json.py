6ca427a02268b0e6a90317c8b457542ce62fa9d8
Natnael Fikadu <88460514+mrnpro@users.noreply.github.com>
2022-06-13 18:59:28 +0300

    Update 8-class_to_json.py

--- 0x0B-python-input_output/8-class_to_json.py
+++ 0x0B-python-input_output/8-class_to_json.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
+"""class_to_json function"""
 
 
 def class_to_json(obj):
-    '''
-        Returns the dict representation of a class
-    '''
-    return (getattr(obj, "__dict__"))
+    """returns the dictionary description"""
+    return obj.__dict__
