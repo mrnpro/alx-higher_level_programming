15ea9b5b0ef24c28b4bb5d2b605e20d2d2e574ed
Natnael Fikadu <88460514+mrnpro@users.noreply.github.com>
2022-06-13 18:54:19 +0300

    Update 3-to_json_string.py

--- 0x0B-python-input_output/3-to_json_string.py
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
+"""to_json_string function"""
+
+
 import json
 
 
 def to_json_string(my_obj):
-    '''
-        Converting an object to a json string
-    '''
-    return (json.dumps(my_obj))
+    """a function that returns the JSON representation of an object (string)"""
+    return json.dumps(my_obj)
